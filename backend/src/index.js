85b6d6a119294158c44276969457d8c9aea0982d
Mykolas Spu <mykolasspu@gmail.com>
2024-05-19 23:03:32 +0300

    Initial commit

---
+++ backend/src/index.js
@@ -0,0 +1,71 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const helmet = require('helmet');
+const morgan = require('morgan');
+
+
+// DATABASE SHIT: //
+const sqlite3 = require ('sqlite3');
+const { Sequelize, DataTypes } = require('sequelize');
+const sequelize = new Sequelize({
+  dialect: 'sqlite',
+  storage: './database.sqlite'
+});
+
+const Item = sequelize.define('Item', {
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false
+  }
+});
+
+sequelize.sync().then(() => {
+  console.log('Database & tables synced!');
+});
+
+
+// END OF DATABASE DECLARATIONS
+const port = 8000;
+
+const app = express();
+
+
+app.use(bodyParser.json());
+app.use(helmet());
+app.use(cors());
+app.use(morgan('combined'));
+
+// MAIN API: //
+
+app.get('/api/items', async (req, res) => {
+  const items = await Item.findAll();
+  res.json(items);
+});
+
+app.post('/api/items', async (req, res) => {
+	if (req.body.auth == "authCode") {
+  		const newItem = await Item.create(req.body.data);
+  		res.json(newItem);
+	}
+});
+
+// END OF MAIN API //
+
+// TEST API: //
+
+app.get('/api/test', async (req, res) => {
+	const test = {title: 'Hello, world!'};
+	res.json(test);
+})
+app.post('/api/test', async (req, res) => {
+	const test = req.body.data;
+	console.log(test)
+	res.json(test);
+})
+
+// END OF TEST API //
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
+});
---
+++ frontend/src/App.js
@@ -0,0 +1,46 @@
+import './App.css';
+
+export default function App () {
+
+	function handleSubmit () {
+		let formData = new FormData (document.querySelector("form"));
+
+		let entries = [...formData.entries()];
+		console.log(entries);
+
+		fetch('http://localhost:8000/api/test', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify(entries[0])
+    	});
+	}
+
+	
+	// fetch('http://localhost:8000/api/test')
+	// 	.then(response => {
+	// 		if (!response.ok)
+	// 			throw new Error("Fetch response not 'ok'")
+	// 		return response.json();
+	// 	})
+	// 	.then(data => {
+	// 		document.getElementById("testPull").textContent=data.title
+	// 	})
+	// 	.catch(error => console.error(error));
+
+	return (
+	<div className='flex flex-auto '>
+		<form className='mx-auto m-4 mt-6 bg-slate-300 w-fit p-2 rounded'>
+			<div className='flex flex-row'>
+				<button type="button" onClick={handleSubmit} className='m-1 p-1 px-2 rounded bg-green-400'> Pateikti </button><br/>
+				<input placeholder='Slaptasis kodas...' type="password" name="authCode" className='m-auto h-6 w-40 py-2' ></input>
+
+			</div>
+			<input placeholder='Pavadinimas...' type="text" name="title" className='h-6 w-80 py-2'/><br/>
+			<input placeholder='Autorius...' type="text" name="author" className='h-6 w-80 py-3'/><br/>
+			<textarea placeholder='Dainos žodžiai...' type="text" name="content" className='h-96 w-80 py-2'/><br/>
+		</form>
+	</div>
+	);
+}
