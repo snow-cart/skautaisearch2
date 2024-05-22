b6c104900b67f91f38599769a13ac66afd06b5a5
Mykolas Spu <mykolasspu@gmail.com>
2024-05-22 16:10:29 +0300

    improve responsive design

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -35,7 +35,11 @@ export function Card ({ data, admin=false, authCode="", className="", handleRefr
 	return(
 		<div className="mx-auto flex flex-row">
 		<div className={`w-[0.5em]`}></div>
-		<div className={className + " flex flex-col rounded bg-slate-300 m-3 p-4"}>
+		<div className={"flex flex-col rounded bg-slate-300 m-3 p-4 border-box "
+						+ "min-w-[60vw] sm:min-w-[40vw] md:min-w-[25vw] lg:min-w-[15vw] "
+						+ " max-w-[250px] sm:max-w-[45vw] md:max-w-[30vw] lg:max-w-[20vw] "
+						+ " " +className}
+					>
 			<div className="flex flex-row w-full">
 				<div className="font-semibold text-lg mr-auto whitespace-nowrap">{`${title}`}</div>
 				{admin && <RemoveBtn itemId={id} className="ml-auto pl-5" authCode={authCode} handleRefresh={handleRefresh}/>}
@@ -51,12 +55,14 @@ export function Card ({ data, admin=false, authCode="", className="", handleRefr
 export default function Cards ({ data, admin=false, authCode, handleRefresh=() => {}}) {
 	console.log("Card collection received: ", data);
 	return (
-		<div className="flex flex-wrap mb-auto w-full">
-			<div className='w-[5vw]'></div>
+		<div className="flex mb-auto w-full">
+			<div className='w-[5vw] md:w-[3vw]'></div>
+			<div className="w-full flex flex-wrap">
 			{data.map(element => (
 				<Card data={element} admin={admin} authCode={authCode} className="mx-auto" handleRefresh={handleRefresh}/>
 			))}
-			<div className='w-[5vw]'></div>
+			</div>
+			<div className='w-[5vw] sm:w-[3vw] md:w-[1vw]'></div>
 		</div>
 	);
 }
