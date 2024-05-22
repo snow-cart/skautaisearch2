8aa69bcb185100225025f6d037288c328f701fc8
Mykolas Spu <mykolasspu@gmail.com>
2024-05-22 17:16:15 +0300

    update searchElement&searchBar to fit current styles, adapt to new Cards functionalitiies

--- frontend/src/components/Search.jsx
+++ frontend/src/components/Search.jsx
@@ -1,7 +1,7 @@
 import Cards from "../components/Card";
 import { useState, useRef } from "react";
 
-export default function SearchElement () {
+export default function SearchElement ({ admin=false, authCode}) {
 
 	const [data, setData] = useState([]);
 
@@ -33,23 +33,31 @@ export default function SearchElement () {
 	return (
 		<div className="flex flex-col w-vw h-svh">
 			<Searchbar onSearch={search}/>
-			<Cards data={data}/>
+			<Cards data={data} admin={admin} authCode={authCode} handleRefresh={search}/>
 		</div>
 	)
 }
 
 function Searchbar ({ onSearch }) {
 	const [searchQuery, setSearchQuery] = useState("");
-	const searchQueryRef = useRef(null);
 
-	function handleClick(){
-		setSearchQuery(searchQueryRef.current.value);
+	function handleClick(event){
+		event.preventDefault();
+		const form = event.currentTarget.parentElement;
+		const formData = new FormData(form)
+		let data = [...formData.entries()];
+		setSearchQuery(data[0][1]);
 		onSearch(searchQuery);
 	}
 	return (
-		<div>
-		<input ref={searchQueryRef}></input>
-		<button onClick={handleClick}>O</button>
-		</div>
-	);
+	<form class="flex items-center max-w-sm mx-auto rounded bg-slate-300 p-2 px-3">
+		<input type="text" name="searchQuery" placeholder="placeholder" className="rounded" required /> 
+
+		<button type="submit" onClick={handleClick} class="p-2.5 ms-2 text-sm font-medium">
+			<svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+				<path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+			</svg>
+			<span class="sr-only">Search</span>
+		</button>
+	</form>);
 }
